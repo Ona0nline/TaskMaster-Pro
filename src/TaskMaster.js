736d92f692bfc2bfabc6f44e6269adaed6f71fe3
Onalerona <onzwanejhb024@student.wethinkcode.co.za>
2024-11-20 11:51:20 +0200

    Trying to remember

---
+++ src/TaskMaster.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TaskMaster.css"
+
+export default function TaskMaster(){
+    return (
+    <div>
+        <h1>Doable ;&#x29;</h1>
+        <button id="login" >Login</button>
+        <button id="signup" >Sign Up</button>
+    </div>)
+    
+}
